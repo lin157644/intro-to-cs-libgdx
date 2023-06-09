998cda97c1b90d556583ebd88c0905408ac9a26b
LXS_TW <19168554+lin157644@users.noreply.github.com>
2023-06-09 15:44:04 +0800

    first commit

---
+++ desktop/src/com/yanglin/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.yanglin.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.yanglin.game.IWantToGraduate;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("I Want To Graduate");
+		new Lwjgl3Application(new IWantToGraduate(), config);
+	}
+}
