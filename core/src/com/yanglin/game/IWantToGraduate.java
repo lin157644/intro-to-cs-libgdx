51d3bd04db88c253a20da6527285d642086e2463
LXS_TW <19168554+lin157644@users.noreply.github.com>
2023-06-09 18:03:31 +0800

    Add stub classes

--- core/src/com/yanglin/game/IWantToGraduate.java
+++ core/src/com/yanglin/game/IWantToGraduate.java
@@ -1,31 +1,44 @@
 package com.yanglin.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class IWantToGraduate extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class IWantToGraduate extends Game {
+    // ApplicationAdapter is more basic than Game
+    // Allow fancier transition
+    
+    public enum Screens {
+        MENU,
+        PLAY,
+        PAUSE,
+        BAD_END,
+        GOOD_END,
+    }
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    SpriteBatch batch;
+    Texture img;
+
+    @Override
+    public void create() {
+        Engine engine = new Engine();
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(1.0f, 1.0f, 1.0f, 0.0f);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
