4078c5389ef35e965def94966ac9838aae5e5d20
LXS_TW <19168554+lin157644@users.noreply.github.com>
2023-06-09 16:10:36 +0800

    Remove unused tilesets

--- assets/tilemaps/2_City_Terrains_16x16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="2_City_Terrains_16x16" tilewidth="16" tileheight="16" tilecount="4307" columns="59">
- <image source="../tilesets/2_City_Terrains_16x16.png" width="944" height="1168"/>
-</tileset>
